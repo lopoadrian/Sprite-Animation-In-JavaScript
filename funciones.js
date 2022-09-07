fc73fe0423897288c78a2661bf6cacee1572c523
Adrian Reggiani <adriregg@gmail.com>
2022-09-07 12:45:05 -0300

    Cambios en el JS
    Se dibujo un rectangulo

--- funciones.js
+++ funciones.js
@@ -1,14 +1,16 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
-const CANVAS_WIDTH = canvas.width = 600;// Se 
+const CANVAS_WIDTH = canvas.width = 600;// Para determinar la medidas del canvas. hay que cambiar el default de CANVAS_Width
 const CANVAS_HEIGHT = canvas.height = 600;
 
-const playerImage = new Image();
+const playerImage = new Image(); // Es como una etiquieta de img.
 playerImage.src = 'shadow_dog';
 
 function animate(){
-    
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); // Limpia el canvas en las coordenadas pasadas.
+    ctx.fillRect(50, 50, 100, 100); // Sirve para dibujar rectangulos. Se pasan coordenadas
+    requestAnimationFrame(animate);
 }
 
-console.log(prueba);
+animate();
