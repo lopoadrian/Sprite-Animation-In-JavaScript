a2f65a95773fb64385eab958b76e341cd70dd361
Adrian Reggiani <adriregg@gmail.com>
2022-09-07 12:05:27 -0300

    Primer Commit
    Inicio de la animacion del personaje

---
+++ funciones.js
@@ -0,0 +1,14 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+const CANVAS_WIDTH = canvas.width = 600;// Se 
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+playerImage.src = 'shadow_dog';
+
+function animate(){
+    
+}
+
+console.log(prueba);
